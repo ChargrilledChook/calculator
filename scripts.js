7bce7834973723adb5223cc4d1e506ae0dc99895
Dylan Williams <happyfrog578+github@gmail.com>
2020-05-15 09:09:38 +1000

    Add event listeners for buttons

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Basic arithmetic functions
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -28,3 +29,88 @@ function operate(input1, operator, input2) {
             return 'Error';        
     }
 }
+
+displayString = ''
+
+// Event listeners for buttons
+const clear = document.querySelector('#clear')
+clear.addEventListener('click', function() {
+
+    displayString = ''
+    display.innerHTML = '0'    
+});
+
+
+const bZero = document.querySelector('#bZero') 
+bZero.addEventListener('click', function() {
+    
+    // Address case of zero being pressed first
+    if (displayString !== '') {
+        displayString += '0';
+        display.innerHTML = displayString;
+    }
+});
+const bOne = document.querySelector('#bOne')
+bOne.addEventListener('click', function() {
+    
+    displayString += '1';
+    display.innerHTML = displayString;
+
+});
+const bTwo = document.querySelector('#bTwo')
+bTwo.addEventListener('click', function() {
+    
+    displayString += '2';
+    display.innerHTML = displayString;
+
+});
+const bThree = document.querySelector('#bThree')
+bThree.addEventListener('click', function() {
+    
+    displayString += '3';
+    display.innerHTML = displayString;
+
+});
+const bFour = document.querySelector('#bFour')
+bFour.addEventListener('click', function() {
+    
+    displayString += '4';
+    display.innerHTML = displayString;
+
+});
+const bFive = document.querySelector('#bFive')
+bFive.addEventListener('click', function() {
+    
+    displayString += '5';
+    display.innerHTML = displayString;
+
+});
+const bSix = document.querySelector('#bSix')
+bSix.addEventListener('click', function() {
+    
+    displayString += '6';
+    display.innerHTML = displayString;
+
+});
+const bSeven = document.querySelector('#bSeven')
+bSeven.addEventListener('click', function() {
+    
+    displayString += '7';
+    display.innerHTML = displayString;
+
+});
+const bEight = document.querySelector('#bEight')
+bEight.addEventListener('click', function() {
+    
+    displayString += '8';
+    display.innerHTML = displayString;
+
+});
+const bNine = document.querySelector('#bNine')
+bNine.addEventListener('click', function() {
+    
+    displayString += '9';
+    display.innerHTML = displayString;
+
+});
+
